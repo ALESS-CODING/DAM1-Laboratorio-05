52318b984210139e3f17e87554cc8148867b0d2e
alex <sedanoalessgmail.com>
2024-06-10 18:54:03 -0500

    commit de laboratorion05 terminado

--- app/src/main/java/com/example/laboratorio05/adapatadores/AdaptadorListaRegistro.kt
+++ app/src/main/java/com/example/laboratorio05/adapatadores/AdaptadorListaRegistro.kt
@@ -1,25 +1,46 @@
 package com.example.laboratorio05.adapatadores
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import com.example.laboratorio05.R
 import com.example.laboratorio05.clases.Registro
 
 class AdaptadorListaRegistro (context: Context?, val listaRegistro: List<Registro>?) : BaseAdapter() {
+
+    private var layoutInflater: LayoutInflater
+
+    init {
+        layoutInflater = LayoutInflater.from(context)
+    }
+
+
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        return  listaRegistro!!.size
     }
 
     override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+        return  listaRegistro!![position]
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+        return position.toLong()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+        var  vista = convertView
+
+        if(vista == null ){
+            //llamamos al layout que queremos carga
+            vista = layoutInflater.inflate(R.layout.elemento_lista_registro, parent, false )
+
+            val objRegistro = getItem(position) as Registro
+
+            //creamos los controles de los elememntos de la lista
+        }
+
+        return  vista!!
     }
 }
