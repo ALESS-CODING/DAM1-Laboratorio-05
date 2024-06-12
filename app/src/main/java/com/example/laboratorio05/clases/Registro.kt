77212015ae9fe645d80753097d408a4bba97044f
aless <sesdanoalessgmail.com>
2024-06-12 16:25:56 -0500

    commit de lab

--- app/src/main/java/com/example/laboratorio05/clases/Registro.kt
+++ app/src/main/java/com/example/laboratorio05/clases/Registro.kt
@@ -1,4 +1,23 @@
 package com.example.laboratorio05.clases
 
+import java.time.LocalDate
+import java.util.Date
+
 class Registro {
+
+    var codigo: Long = 0
+    var nombre: String = ""
+    var apellidos : String =""
+    var dni: String = ""
+    var fecha: LocalDate? = null
+    var direccion: String = ""
+    var telefono: String = ""
+    var celular: String = ""
+    var correo: String = ""
+    var sexo: String = ""
+    var estado: Boolean = false
+    var distrito: Distrito ? = null
+
+
+
 }
